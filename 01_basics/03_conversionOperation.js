4ec75e33343e4106d819d1c1a4518d96a6b2f26c
TabishNeyaz <114953669+TabishNeyaz@users.noreply.github.com>
2023-09-17 07:17:36 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Tabish";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0
+// null => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 
+// 0 => fasle
+// "" => false because "" is falsy value
+// "abc" => true  
+
+let someNumber = 27;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
