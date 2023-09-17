5662f8e0e50fa09b5cde7312e561be8aa0d173fa
TabishNeyaz <114953669+TabishNeyaz@users.noreply.github.com>
2023-09-17 17:10:47 +0000

    dataType_summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,59 @@
+// Primitive
+
+/* 7 types
+1. String
+2. Number
+3. Boolean
+4. Null
+5. Undefined
+6. Symbol
+7. BigInt
+*/
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = true;
+
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id == anotherId);
+// console.log(id === anotherId);
+
+const bigNumber = 454656465454564564888458745n;
+
+// Reference (Non-primitive)
+
+/*
+1. Array
+2. Objects
+3. Functions etc..
+*/
+
+const heros = ["Ironman","Spiderman","Flash"];
+
+let myObj = {
+    name: "Tabish",
+    age : 27,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+} 
+
+console.log(typeof bigNumber);
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof anotherId);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+// myFunction();
+
+
