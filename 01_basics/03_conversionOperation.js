6797e90ab44143870f2c209d17b512e84c6a3e72
TabishNeyaz <114953669+TabishNeyaz@users.noreply.github.com>
2023-09-17 16:24:01 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 27;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********** Operations ***************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(15/3);
+// console.log(15%4);
+
+let str1 = "Hello";
+let str2 = " Tabish";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 >1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//The reason is that equality check == and comparison > < >= <= work 
+//differently.
+//Comparisons convert null to a number,treating it as 0.
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+
+// ===
+
+console.log("20" === 20);
+
