e721d40e10dee2b418c3f6964eb25d02ec433598
TabishNeyaz <114953669+TabishNeyaz@users.noreply.github.com>
2023-09-29 06:34:46 +0000

    JSON

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor: "hitesh"
+}
+
+console.log(course.courseInstructor);
+
+const  {courseInstructor: instructor} = course;
+
+console.log(instructor);
+
+// JSON Object
+
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price":"free"
+// }
+
+
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
